e04d0d7db579f48804fd077c9e20f3bfc2798950
Kamil <kamilkoziol529@gmail.com>
2025-08-18 10:12:58 +0200

    add new animations and responsice scss

--- src/JAVASCRIPT/animations.js
+++ src/JAVASCRIPT/animations.js
@@ -29,3 +29,87 @@ export function hideLightbox() {
 		ease: 'back.inOut',
 	});
 }
+
+export function startAnimation() {
+	gsap.from('.container header .img', {
+		x: -100,
+		opacity: 0,
+		scale: 0,
+		duration: 0.4,
+		ease: 'power2.inOut',
+	});
+	gsap.from('.container header .info-person-right-container .top-bar-person-name', {
+		y: 100,
+		opacity: 0,
+		scale: 0,
+		duration: 0.6,
+		delay: 0.2,
+		ease: 'power2.inOut',
+	});
+	gsap.from('.container header .info-person-right-container .info-row-bar-numbers', {
+		y: -100,
+		opacity: 0,
+		scale: 0,
+		duration: 0.6,
+		delay: 0.55,
+		ease: 'power2.inOut',
+	});
+	gsap.from('.container header .info-person-right-container .col-name h2', {
+		y: 100,
+		opacity: 0,
+		scale: 0,
+		duration: 0.6,
+		delay: 0.75,
+		ease: 'power2.inOut',
+	});
+	gsap.from('.container header .info-person-right-container .col-name p', {
+		y: 100,
+		opacity: 0,
+		scale: 0,
+		duration: 0.4,
+		delay: 0.85,
+		ease: 'power2.inOut',
+	});
+	gsap.from('.container header .info-person-right-container .col-name span', {
+		y: 100,
+		opacity: 0,
+		scale: 0,
+		duration: 0.4,
+		delay: 0.95,
+		ease: 'power2.inOut',
+	});
+
+	gsap.fromTo(
+		'.story-container .card',
+		{
+			y: 100,
+			opacity: 0,
+			scale: 0,
+		},
+		{
+			y: 0,
+			opacity: 1,
+			scale: 1,
+			duration: 0.4,
+			delay: 0.9,
+			ease: 'power2.inOut',
+			stagger: 0.2,
+		}
+	);
+
+	gsap.from('.photos-container .photo', {
+		opacity: 0,
+		scale: 0,
+		duration: 0.4,
+		delay: 1,
+		ease: 'power2.inOut',
+	});
+	gsap.from('.main-photos h2', {
+		y: -100,
+		opacity: 0,
+		scale: 0,
+		duration: 0.45,
+		delay: 1.65,
+		ease: 'power2.inOut',
+	});
+}
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import openLighbox from './JAVASCRIPT/mainOpenLighbox';
 import toggleTheme from './JAVASCRIPT/toggleTheme';
 import renderApp from './JAVASCRIPT/renderApp';
 import './SCSS/style.scss';
+import { startAnimation } from './JAVASCRIPT/animations';
 
 renderApp();
 openLighbox();
 toggleTheme();
 savedRelations();
+startAnimation();
