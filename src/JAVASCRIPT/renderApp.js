309c98fd52ee1ed3dda988839a2c69ac73b00914
Kamil <kamilkoziol529@gmail.com>
2025-08-18 10:18:31 +0200

    nl flag fix path

--- src/JAVASCRIPT/renderApp.js
+++ src/JAVASCRIPT/renderApp.js
@@ -86,9 +86,9 @@ export default function renderApp() {
 					<div class="img"></div>
 					<div class="country-span">
 						<span class="country-name">Netherland</span>
-						<img src="public/photos/flags/netherlands.png" alt="" />
-						<img src="public/photos/flags/netherlands.png" alt="" />
-						<img src="public/photos/flags/netherlands.png" alt="" />
+						<img src="photos/flags/netherlands.png" alt="" />
+						<img src="photos/flags/netherlands.png" alt="" />
+						<img src="photos/flags/netherlands.png" alt="" />
 					</div>
 				</div>
 			</article>
